768ee82ee1523f1c249726162f7ea4ea500da53d
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-08 16:25:20 +0545

    next conf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: "@svgr/webpack",
+		})
+		return config
+	},
+	images: {
+		domains: ["portswigger.net", "cdn.myanimelist.net"],
+	},
 }
 
 module.exports = nextConfig
