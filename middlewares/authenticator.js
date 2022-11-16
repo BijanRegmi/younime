7facacdf0d8de88ef8016e18c1e47c16de4b1912
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2022-11-17 00:40:10 +0545

    middleware adapted to next-auth

--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -1,14 +1,14 @@
-import nookies from "nookies"
-import { verifyToken } from "../utils/jwt"
+import { unstable_getServerSession } from "next-auth"
+import { authOptions } from "../pages/api/auth/[...nextauth]"
 
 const authenticator = next => {
-	return (req, res) => {
-		const { token } = nookies.get({ req })
-		const { success, payload, message } = verifyToken(token)
+	return async (req, res) => {
+		const session = await unstable_getServerSession(authOptions)
 
-		if (!success) return res.status(400).json({ message })
-		req.tokenObj = payload
+		if (!session.user)
+			return res.status(400).json({ message: "Not logged in." })
 
+		req.user = session.user
 		return next(req, res)
 	}
 }
