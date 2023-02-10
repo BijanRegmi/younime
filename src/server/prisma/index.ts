c3c5368bf22daf63e40743fd65817ab0b65474a8
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-02-10 21:19:16 +0545

    move api logic to controllers

---
+++ src/server/prisma/index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+
+const prismaGlobal = global as typeof global & {
+	db?: PrismaClient
+}
+
+const db: PrismaClient =
+	prismaGlobal.db ||
+	new PrismaClient({
+		log: ["info"],
+	})
+
+if (process.env.NODE_ENV !== "production") {
+	prismaGlobal.db = db
+}
+
+export default db
