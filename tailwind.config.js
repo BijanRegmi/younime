6608dceb393c1f1e18116e9974c6f4d03ce8eef5
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-02-15 10:39:07 +0545

    tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx}",
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
 
-		// Or if using `src` directory:
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                accent: {
+                    50: "#000000ff",
+                    100: "#0A0A0Aff",
+                    150: "#141414ff",
+                    200: "#1E1E1Eff",
+                    250: "#282828ff",
+                    300: "#323232ff",
+                    350: "#3C3C3Cff",
+                    400: "#464646ff",
+                    450: "#505050ff",
+                    500: "#5A5A5Aff",
+                    550: "#696969ff",
+                    600: "#7E7E7Eff",
+                    650: "#949494ff",
+                    700: "#A9A9A9ff",
+                    750: "#BFBFBFff",
+                    800: "#D4D4D4ff",
+                    850: "#EAEAEAff",
+                    900: "#FFFFFFff",
+                },
+            },
+        },
+    },
+    plugins: [],
 }
